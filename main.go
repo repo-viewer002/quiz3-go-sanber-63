b6264d64b9d460466e881cba17c2af9fa7794eb4
samm002 <samuelsdjodis@gmail.com>
2025-01-27 08:06:53 +0700

    feat: add index route for project informations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"quiz3/commons"
 	"quiz3/configs/database"
 	"quiz3/middlewares"
@@ -19,6 +20,8 @@ func main() {
 	router := gin.Default()
 	router.Use(middlewares.Log())
 
+	router.GET("/", indexController)
+
 	categories.CategoryRouter(router)
 	books.BookRouter(router)
 	users.UserRouter(router)
@@ -26,4 +29,32 @@ func main() {
 	swagger.Initiator(router)
 
 	router.Run(fmt.Sprintf(":%d", commons.PORT))
+}
+
+func indexController(ctx *gin.Context) {
+	scheme := "http"
+	if ctx.Request.TLS != nil {
+		scheme = "https"
+	}
+
+	host := ctx.Request.Host
+
+	apiDocumentation := scheme + "://" + host + "/swagger/index.html"
+
+	ctx.Data(http.StatusOK, "text/html", []byte(`
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>API Documentation</title>
+			</head>
+			<body>
+				<h1>Sanbercode Golang Backend Development | Quiz 3</h1>
+				<a href="`+apiDocumentation+`" target="_blank">API Documentation</a></br>
+				<a href="`+commons.REPOSITORY+`" target="_blank">Github Repository</a></br>
+				<a href="`+commons.ENDPOINT+`" target="_blank">Railway Deployment URL</a>
+			</body>
+			</html>
+		`))
 }
